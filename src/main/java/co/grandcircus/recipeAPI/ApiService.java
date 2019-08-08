99023b9b60f9c8af7b908dbb4e610a4d1c5ea648
sandya <sandyavasan@gmail.com>
2019-08-08 13:14:09 -0400

    Initial commit

---
+++ src/main/java/co/grandcircus/recipeAPI/ApiService.java
@@ -0,0 +1,18 @@
+package co.grandcircus.recipeAPI;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestTemplate;
+
+public class ApiService {
+
+	private RestTemplate restTemplate;
+	{
+		ClientHttpRequestInterceptor interceptor = (request, body, execution) -> {
+			request.getHeaders().add(HttpHeaders.USER_AGENT, "Spring");
+			return execution.execute(request, body);
+		};
+		restTemplate = new RestTemplateBuilder().additionalInterceptors(interceptor).build();
+	}
+}
---
+++ src/test/java/co/grandcircus/recipeAPI/RecipeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package co.grandcircus.recipeAPI;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
